11d2469db407c41ceb7bf75c3302371500718f85
Ryan Scott <rsmease@gmail.com>
2017-09-25 16:35:28 +0000

    Sep 25 17

--- addTwoDigits.js
+++
@@ -1,3 +0,0 @@
-function addTwoDigits(n) {
-	return Math.floor(n / 10) + (n % 10);
-}
--- alphabetPositions.js
+++ alphabetPositions.js
@@ -1,13 +1,13 @@
+//returns positions of all letters in string
+//refactored for ES6, Sep 25 17
 function alphabet_position(text) {
-	var newText = [];
-	var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-	for (var i = 0; i < text.length; i++) {
-		var currentLower = text.toLowerCase().charAt(i);
-		if (alphabet.indexOf(currentLower) !== -1) {
-			newText.push(alphabet.indexOf(currentLower) + 1);
-		}
-	}
-	return newText.join(" ");
+	var alphabet = [ ..."abcdefghijklmnopqrstuvwxyz" ]
+	var alphaArr = getAlphaArr(text.toLowerCase());
+	return alphaArr.map(ele => alphabet.indexOf(ele) + 1);
+}
+
+function getAlphaArr(text) {
+	return text.split("").filter(ele => /[A-Za-z]/.test(ele));
 }
 
 console.log(alphabet_position("The sunset sets at twelve o' clock."))

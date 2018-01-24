b473127c8781a6533cac74a21f69193b91e9c1fa
Ryan Mease <rsmease@gmail.com>
2018-01-24 12:01:42 -0800

    update MergeLinkedLists

--- mergeLinkLists.rb
+++ mergeLinkLists.rb
@@ -11,23 +11,45 @@ def mergeTwoLinkedLists(l1, l2)
     head = l3
     loop do
         if l1.nil? || l2.nil? 
-            return head
-        end
-        if l1.value.nil? && l2.value.nil?
-            return head
-        elsif l2.value.nil?
-            l3.value = l1.value
-            return head
-        elsif l1.value.nil?
-            l3.value = l2.value 
-            return head
-        else
-            if l1.value <= l2.value
-                l3.value = l1.value
+            puts "l1 is nil" if l1.nil?
+            puts "l2 is nil" if l2.nil?
+            if l1.nil? && l2.nil? 
                 return head
-            else 
+            elsif l1.nil?
                 l3.value = l2.value
+                l2 = l2.next ? l2.next : nil
+                unless l2.nil?
+                    l3.next = ListNode.new(nil)
+                    l3 = l3.next
+                end
+            else
+                puts "l2 is nil"
                 return head
+            end
+        else 
+            if l1.value.nil? && l2.value.nil?
+                return head
+            elsif l2.value.nil?
+                l3.value = l1.value
+                l3.next = l1.next
+                return head
+            elsif l1.value.nil?
+                l3.value = l2.value
+                l3.next = l2.next
+                return head
+            else
+                if l1.value <= l2.value
+                    l3.value = l1.value
+                    l1 = l1.next ? l1.next : nil
+                    l3.next = ListNode.new(nil)
+                    l3 = l3.next
+                else 
+                    l3.value = l2.value
+                    l2 = l2.next ? l2.next : nil
+                    l3.next = ListNode.new(nil)
+                    l3 = l3.next
+                    return head
+                end
             end
         end
     end

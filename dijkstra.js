61d355ae7d09598914edb7f3c853cc39c73bf81f
Ryan Mease <rsmease@gmail.com>
2018-02-19 10:26:41 -0800

    Create file for dijkstra practice

---
+++ dijkstra.js
@@ -0,0 +1,11 @@
+//this is not an original implementation
+//this was written to practice the algorithm
+//code is based on:
+//https://github.com/mburst/dijkstras-algorithm/blob/master/dijkstras.js
+
+class PriorityQ() {
+    constructor(
+        this.nodes = [];
+
+    )
+}

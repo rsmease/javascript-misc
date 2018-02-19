469abeca036eea6bc18f318a6d3526896572f203
Ryan Mease <rsmease@gmail.com>
2018-02-19 12:17:49 -0800

    finish mature priority queue

--- priorityQueue.js
+++ priorityQueue.js
@@ -46,6 +46,7 @@ class PriorityQueue {
         this.heap = [null];
     }
 
+    //now worst case runtime is O(log(n))
     insert(value, priority) {
         const newNode = new Node(value, priority);
         this.heap.push(newNode);
@@ -55,7 +56,33 @@ class PriorityQueue {
             this.heap[currentNodeParentIndex] &&
             newNode.priority > this.heap[currentNodeParentIndex].priority
         ) {
-
+            const parent = this.heap[currentNodeParentIndex];
+            this.heap[currentNodeParentIndex] = newNode;
+            this.heap[currentNodeIndex] = parent;
+            currentNodeIndex = currentNodeParentIndex;
+            currentNodeParentIndex = Math.floor(currentNodeIndex / 2);
         }
+    }
+
+    remove() {
+        let toReturn;
+        //we will replace the element with itself if we have an array of length one or two, using the pattern below, so we handle the special case in this way:
+        if (this.heap.length < 3) {
+            toReturn = this.heap.pop();
+            this.heap[0] = null;
+            return toReturn;
+        }
+        toReturn = this.heap[1];
+        this.heap[1] = this.heap.pop();
+        let currentIndex = 1;
+        let [left, right] = [2 * currentIndex, 2 * currentIndex + 1];
+        let currentChildIndex = this.heap[right] && this.heap[right].priority > this.heap[left].priority ? right : left;
+        while (this.heap[currentChildIndex] && this.heap[currentIndex].priority < this.heap[currentChildIndex].priority) {
+            let currentNode = this.heap[currentIndex];
+            let currentChildNode = this.heap[currentChildIndex];
+            this.heap[currentChildNode] = currentNode;
+            this.heap[currentIndex] = currentChilNode;
+        }
+        return toReturn;
     }
 }
